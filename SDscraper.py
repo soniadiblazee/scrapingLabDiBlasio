ba1a7b3c150a23edf3c5ffe817545e799acf6316
soniadiblazee <cindylol.11037@gmail.com>
2024-10-03 10:19:50 -0400

    Update SDscraper.py
    code

--- SDscraper.py
+++ SDscraper.py
@@ -1,40 +1,24 @@
-import random
 import requests
+from bs4 import BeautifulSoup
 
-# URL for API
-base_url = "https://swapi.dev/api/people/"
+# url of weather underground location:
+url = "https://www.wunderground.com/weather/us/oh/youngstown"
 
+# send http request to fetch html content of the page:
+response = requests.get(url)
 
-# fetch data for the first character in the API (Luke Skywalker)
-response = requests.get(base_url + "1/")
-character_data = response.json()
+# INITIALIZE BEAUTIFULSOUP :fire::fire::explosion:
+soup = BeautifulSoup(response.text, "html.parser")
 
+# print status code
+print(f"status code: {response.status_code}")
+# print parsed html (optimal to use for page structure !!!)
+#! print(soup.prettify())
 
-# print the character's name
-print("Character Name:", character_data['name'])
+#* temperatureElement = soup.find("span", class_="wu-value wu-value-to")
+temperatureElement = soup.find_all("span", class_="wu-value wu-value-to")
+#* temperature = temperatureElement.text if temperatureElement else "N/A"
+for element in temperatureElement:
+    print(element.text)
 
-# generate a random number between 1 and 83
-random_character_id = random.randint(1, 83)
-
-
-# fetch data for the randomly selected character
-response = requests.get(base_url + str(random_character_id) + "/")
-character_data = response.json()
-
-
-# Print the character's name and other details
-print("Randomly Selected Character:", character_data['name'])
-print("Character Height:", character_data['height'], "cm")
-print("Character Mass:", character_data['mass'], "kg")
-print("Character Birth Year:", character_data['birth_year'])
-
-# Select 3 random Star Wars characters
-for i in range(3):
-   random_character_id = random.randint(1, 83)
-   response = requests.get(base_url + str(random_character_id) + "/")
-   character_data = response.json()
-  
-   print(f"\nCharacter {i+1}: {character_data['name']}")
-   print("Height:", character_data['height'], "cm")
-   print("Mass:", character_data['mass'], "kg")
-   print("Birth Year:", character_data['birth_year'])
+# print(f"current temp: {temperature}")
